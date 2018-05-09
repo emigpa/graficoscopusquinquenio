2fb985075bd85098d5028073012ebee9b06174d2
emigpa <emigpa@gmail.com>
2018-05-09 17:58:45 -0300

    Refactored app structure. Optimized imports. Changed layout and colors.

---
+++ src/AppOld.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container fluid>
+    <v-layout wrap column>
+      <v-flex>
+      <chart></chart>
+    </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+import Chart from './Chart.vue'
+export default {
+  components: {
+    'chart': Chart
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style>
+body {
+  font-family: 'Lato';
+}
+.application {
+  font-family: 'Lato';
+}
+.chart-container {
+  font-family: 'Lato';
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,36 @@
 import Vue from 'vue/dist/vue.min.js'
-import Vuetify from 'vuetify/dist/vuetify.min.js'
 import App from './App.vue'
 import 'vuetify/dist/vuetify.min.css'
-// import Chartkick from 'chartkick'
-// import VueChartkick from 'vue-chartkick'
-// import Chart from 'chart.js/dist/Chart.min.js'
+import {
+  Vuetify,
+  VApp,
+  VCard,
+  VList,
+  VDivider,
+  VSwitch,
+  VGrid,
+  VFooter,
+  VBtn,
+  VDialog,
+  VIcon
+  // transitions
+} from 'vuetify'
 
-Vue.use(Vuetify)
-// Vue.use(VueChartkick, { Chartkick })
-// Chartkick.configure({language: 'es'})
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VCard,
+    VList,
+    VDivider,
+    VSwitch,
+    VGrid,
+    VFooter,
+    VBtn,
+    VDialog,
+    VIcon
+    // transitions
+  }
+})
 
 const app = new Vue({
   el: '#app',
